fe218342d01d5fe3cc4954e2e5b361a7d6d2c41d
trongt16 <trongt16@uia.no>
2017-10-09 15:24:40 +0200

    Added plog submodule

--- AlpacGame/main.cpp
+++ AlpacGame/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <plog\log.h>
 #include <SFML/Graphics.hpp>
 
 int main() {
+        
+    plog::init(plog::debug,"Log.log");
+    LOGI << "Initiating plog.";
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
