e38912ead9908d34f56ae23a19b6757fd0c552ea
Sander Takvam <sandet16@student.uia.no>
2017-10-09 14:57:33 +0200

    Created a functioning menu with choices

--- AlpacGame/main.cpp
+++ AlpacGame/main.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <plog\log.h>
 #include <SFML/Graphics.hpp>
-#include "window.h"
 #include "state/StateMachine.h"
+#include "window.h"
+#include "main_menu.h"
 
-int main() {
+int main()
+{
 
     plog::init(plog::debug,"Log.log");
     LOGI << "Initiating plog.";
@@ -17,7 +19,61 @@ int main() {
         stateMachine.proceed();
     }
 
-    window window;
+    main_menu menu;
 
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "AlpacGame");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type) {
+                case sf::Event::KeyPressed : {
+                    switch (event.key.code) {
+                        case sf::Keyboard::Up : {
+                            menu.moveUp();
+                            break;
+                        }
+                        case sf::Keyboard::Down : {
+                            menu.moveDown();
+                            break;
+                        }
+                        case sf::Keyboard::Return :
+                        {
+                            switch (menu.GetPressedItem())
+                            {
+                                case 0 :
+                                std::cout << "Play" << std::endl;
+                                    break;
+
+                                case 1 :
+                                std::cout << "Options" << std::endl;
+                                    break;
+
+                                case 2 :
+                                window.close();
+
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+
+        window.draw(menu.menu[0]);
+        window.draw(menu.menu[1]);
+        window.draw(menu.menu[2]);
+
+
+        window.display();
+    }
 }
