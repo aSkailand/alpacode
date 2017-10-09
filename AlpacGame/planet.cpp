0e1d35be62dbb8dfc19f400a3181287987ed4480
Aslak Frafjord Skailand <aslaks16@uia.no>
2017-10-09 19:22:38 +0200

    fuck

--- AlpacGame/planet.cpp
+++ AlpacGame/planet.cpp
@@ -2,45 +2,59 @@
 
 
 void planetClass::planet_controll() {
-
-    int radius = 1000;
-    int xpos = 0, ypos = 0;
-    sf::RenderWindow alpacaGame(sf::VideoMode(3000, 1300), "Hello, World!");
-
-
-    sf::CircleShape planet(radius);
+    // loads texture
     sf::Texture texture;
-
-
-    sf::RectangleShape entity(sf::Vector2f(100, 100));
-    entity.setSize(sf::Vector2f(100, 100));
-    entity.setFillColor(sf::Color::Red);
-
-    if (!texture.loadFromFile("earth.png")) {
+    if (!texture.loadFromFile("wat.png")) {
         std::cout << "Error!!!" << std::endl;
     }
 
+    sf::Texture alpaca;
+    if (!alpaca.loadFromFile("alpaca.png")) {
+        std::cout << "Error!!!" << std::endl;
+    }
+
+    sf::Texture background;
+    if (!background.loadFromFile("back.png")) {
+        std::cout << "Error!!!" << std::endl;
+    }
+
+    background.setSmooth(true);
+
+    sf::Sprite back(background);
+
+
+
+    int radius = 1000;
+    int xpos = 0, ypos = 0;
+
+    sf::RenderWindow alpacaGame(sf::VideoMode(800, 600), "Alpaca Worlds!");
+
+    sf::CircleShape planet(radius);
     planet.setTexture(&texture);
-    planet.setTextureRect(sf::IntRect(100, 100, 100, 100));
+
     planet.move(1280 / 2, 1300);
     planet.setOrigin(sf::Vector2f(1000, 1000));
 
 
+    sf::RectangleShape entity(sf::Vector2f(0,0 ));
+    entity.setSize(sf::Vector2f(200, 200));
+    entity.setTexture(&alpaca);
+    entity.rotate(90);
+
     while (alpacaGame.isOpen()) {
 
         sf::Event event;
-
         while (alpacaGame.pollEvent(event)) {
-
             if (event.type == sf::Event::Closed) {
                 alpacaGame.close();
             }
-
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
                 planet.rotate(1);
+                entity.rotate(1);
                 std::cout << planet.getRotation() << std::endl;
             } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
                 planet.rotate(-1);
+                entity.rotate(-1);
                 std::cout << planet.getRotation() << std::endl;
             } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) {
                 std::cout << "Information: " << std::endl;
@@ -49,16 +63,16 @@ void planetClass::planet_controll() {
                 std::cout << "YPOS is: " << ypos << std::endl;
             }
 
-            //calculate x pos
-
-            xpos = (int) (radius * (std::cos((planet.getRotation() * M_PI / 180))));
-            ypos = (int) (radius * (std::sin((planet.getRotation() * M_PI / 180))));
+            //calculate X and Y position
+            xpos = (int) 1280 / 2 + ((radius+150) * (std::cos((planet.getRotation() * M_PI / 180))));
+            ypos = (int) 1300 + ((radius+150) * (std::sin((planet.getRotation() * M_PI / 180))));
 
 
             entity.setPosition(xpos, ypos);
 
-
             alpacaGame.clear(sf::Color::Cyan);
+
+            alpacaGame.draw(back);
 
             alpacaGame.draw(planet);
             alpacaGame.draw(entity);
