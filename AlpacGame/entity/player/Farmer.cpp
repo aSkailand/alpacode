9bfdd882d6afde8577235527e6bb704bfc5ca026
trongt16 <trongt16@uia.no>
2017-10-15 19:58:05 +0200

    Added foundation for character, still very unfinished.

---
+++ AlpacGame/entity/player/Farmer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Trong on 12/10/2017.
+//
+
+#include "Farmer.h"
+
+Farmer::Farmer() {
+
+    // Load texture
+
+
+}
---
+++ AlpacGame/entity/player/Farmer.h
@@ -0,0 +1,21 @@
+//
+// Created by Trong on 12/10/2017.
+//
+
+#ifndef ALPACGAME_FARMER_H
+#define ALPACGAME_FARMER_H
+
+#include <SFML/Graphics/RectangleShape.hpp>
+
+class Farmer {
+public:
+
+    Farmer();
+
+    sf::RectangleShape rectangle;
+
+private:
+};
+
+
+#endif //ALPACGAME_FARMER_H
